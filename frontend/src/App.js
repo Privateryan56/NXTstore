06524f07252e440263c41c68b1a211b11880a636
Austin bambino <link9215@gmail.com>
2022-07-13 00:28:31 -0400

    added navbar aswell as a bootswatch which is cool and ez

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-    <h1> Hello, there</h1>
+    <Header/>
+      <main className='py-3'>
+        <Container>
+        <h1> Hello, there</h1>
+        </Container>
+        </main>
+      <Footer/>
     </>
   );
 }
