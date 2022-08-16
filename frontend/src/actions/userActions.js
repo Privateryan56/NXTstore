b18cab543dfcd004495af7e0c5dfdb252eda652f
Austin bambino <link9215@gmail.com>
2022-08-15 21:35:53 -0400

    I hate login forms and user authentication as well as redux... in other news login Screen was made

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,34 @@
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_OUT } from "../constants/userConstants"
+import axios from "axios"
+
+export const login = (email, password) => async(dispatch) => {
+    try{
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+        const config = {
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }
+        const {data} = await axios.post(
+            '/api/users/login',
+            {email,password},
+            config
+        )
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+        localStorage.setItem('userInfo', JSON.Stringify(data))
+    } catch(error){
+
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload:
+              error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+          });
+    }
+}
