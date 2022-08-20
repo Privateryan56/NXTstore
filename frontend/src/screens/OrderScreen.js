65592ee9fc072ebe52f0f46fab30073333271a91
unknown <danieleastwood4@gmail.com>
2022-08-20 09:41:54 -0400

    order screen

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -21,6 +21,15 @@ function OrderScreen() {
   const orderDetails = useSelector((state) => state.orderDetails);
   const { order, loading, error } = orderDetails;
 
+  if (!loading) {
+    const addDecimals = (num) => {
+      return (Math.round(num * 100) / 100).toFixed(2);
+    };
+    order.itemsPrice = addDecimals(
+      order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+    );
+  }
+
   useEffect(() => {
     dispatch(getOrderDetails(orderId));
   }, [success]);
